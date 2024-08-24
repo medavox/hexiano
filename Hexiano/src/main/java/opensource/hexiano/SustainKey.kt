bcaf43f996ee28c9f1fa326e613c66d9df9138d2
Adam Howard <medavox2+github@gmail.com>
2024-08-24 16:39:42 +0100

    convert SustainKey to Kotlin

--- Hexiano/src/main/java/opensource/hexiano/SustainKey.kt
+++ Hexiano/src/main/java/opensource/hexiano/SustainKey.kt
@@ -14,7 +14,7 @@
  *   3 of the License, or (at your option) any later version.              *
  *                                                                         *
  *   Hexiano is distributed in the hope that it will be useful,            *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   but WITHOUT ANY WARRANTY without even the implied warranty of        *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
@@ -22,59 +22,45 @@
  *   along with Hexiano.  If not, see <http://www.gnu.org/licenses/>.      *
  *                                                                         *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-package opensource.hexiano;
+package opensource.hexiano
 
-import android.content.Context;
-import android.util.Log;
+import android.content.Context
+import android.util.Log
 
-public class SustainKey extends ModifierKey
-{
-	public SustainKey(Context context, int radius, Point center,
-			int midiNoteNumber, Instrument instrument, int keyNumber)
-	{
-		super(context, radius, center, 64, instrument, keyNumber);
-	}
-	
-	@Override
-	public int getColor() {
-		int color = mWhiteColor;
-		if (mSpecialColor != 0) {
-			color = mSpecialColor;
-		}
-		
-		return color;
-	}
-	
-	@Override
-	public void play()
-	{
-		if (HexKeyboard.mSustainHold == true && this.getPressed() == true) {
-			this.stop(true);
+class SustainKey(
+	context: Context,
+	radius: Int,
+	center: Point,
+	midiNoteNumber: Int,
+	instrument: Instrument,
+	keyNumber: Int
+): ModifierKey(context, radius, center, 64, instrument, keyNumber) {
+	override fun getColor(): Int = if (mSpecialColor != 0) mSpecialColor else mWhiteColor
+
+	override fun play() {
+		if (HexKeyboard.mSustainHold == true && getPressed() == true) {
+			stop(true)
 		} else {
-			HexKeyboard.mSustain = true;
-			String pitchStr = String.valueOf(mMidiNoteNumber);
-			Log.d("HexKey::play", pitchStr);
-			this.setPressed(true);
+			HexKeyboard.mSustain = true
+			val pitchStr = mMidiNoteNumber.toString()
+			Log.d("HexKey::play", pitchStr)
+			pressed = true
 		}
-		return;
 	}
 	
-	@Override
-	public void play(float pressure)
-	{
-		this.play();
+	override fun play(pressure: Float) {
+		this.play()
 	}
 	
-	@Override
-	public void stop(boolean force)
-	{
-		if (this.getPressed() == true && (force == true | HexKeyboard.mSustainHold == false)) { // TODO: find why not all notes stops sometimes when sustain is released (whether mSustainHold is on or off doesn't matter for this bug)
-			String pitchStr = String.valueOf(mMidiNoteNumber);
-			Log.d("HexKey::stop", pitchStr);
-			this.setPressed(false);
-			HexKeyboard.mSustain = false;
-			HexKeyboard.stopAll(); // stop all previously sustained notes
+	override fun stop(force: Boolean) {
+		// TODO: find why not all notes stops sometimes when sustain is released (whether mSustainHold is on or off doesn't matter for this bug)
+		if (this.getPressed() == true && (force == true or HexKeyboard.mSustainHold == false)) {
+			val pitchStr = mMidiNoteNumber.toString()
+			Log.d("HexKey::stop", pitchStr)
+			pressed = false
+			HexKeyboard.mSustain = false
+			HexKeyboard.stopAll() // stop all previously sustained notes
 		}
-		return;
+		return
 	}
 }
